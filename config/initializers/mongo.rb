f9673129b27f96609cd236a1ce03019dd759426c
Kostas Karachalios <kostas.karachalios@gmail.com>
2012-02-18 00:13:19 +0200

    Wrote a quick and dirty fix for heroku deployment

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,6 +1,11 @@
-MongoMapper.config = {
-  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 'mongodb://localhost/sushi' }
-}
+begin
+  MongoMapper.config = {
+    Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 'mongodb://localhost/sushi' }
+  }
 
-MongoMapper.connect(Rails.env)
-MongoMapper::Document.plugin(MongoMapper::Plugins::IdentityMap)
+  MongoMapper.connect(Rails.env)
+  MongoMapper::Document.plugin(MongoMapper::Plugins::IdentityMap)
+rescue => ex
+  # should only rescue on 'rake assets:precompile:all'
+  puts ex.message + " but it's ok, I still love you"
+end
