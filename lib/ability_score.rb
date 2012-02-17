0d5eeec9f9d84b5935ce4b0760c616132215dbeb
Kostas Karachalios <kostas.karachalios@gmail.com>
2012-02-17 17:40:24 +0200

    fixed AbilityScore for MongoMapper

--- lib/ability_score.rb
+++ lib/ability_score.rb
@@ -7,9 +7,17 @@ class AbilityScore
     (@value-10)/2
   end
 
+  def to_s
+    @value.to_s
+  end
+
   # needed for MongoMapper to load the appropriate class
-  def self.from_mongo(value)
-    self.new(value)
+  def self.from_mongo(integer)
+    self.new(integer)
+  end
+
+  def self.to_mongo(ability_score)
+    ability_score.to_i
   end
 
   # delegate the rest of the functions to the @value
@@ -19,9 +27,5 @@ class AbilityScore
     else
       @value.send(method_name, *args)
     end
-  end
-
-  def to_s
-    @value.to_s
   end
 end
