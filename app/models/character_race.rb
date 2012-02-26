b9392ec8686ac3c786276a1a90e1f52ea57a2411
Kostas Karachalios <kostas.karachalios@gmail.com>
2012-02-26 23:54:04 +0200

    CharacterRace now has a description

--- app/models/character_race.rb
+++ app/models/character_race.rb
@@ -4,6 +4,8 @@ class CharacterRace
   key :name, String
   validates_presence_of :name
 
+  key :description, String
+
   # TODO should probably move this to a separate decorator soon
   def self.for_select
     all.map{|r| [r.name, r.id]}
