40fbd52ec05a2f9e33f4285f08d659176325cae3
Kostas Karachalios <kostas.karachalios@gmail.com>
2012-02-26 14:38:35 +0200

    Moved some code away from the form view
    moved inside of CharacterRace model but will probably be moved to a separate decorator soon

--- app/models/character_race.rb
+++ app/models/character_race.rb
@@ -2,4 +2,9 @@ class CharacterRace
   include MongoMapper::Document
 
   key :name, String
+
+  # TODO should probably move this to a separate decorator soon
+  def self.for_select
+    all.map{|r| [r.name, r.id]}
+  end
 end
