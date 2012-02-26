d030f3a8b8773ef97225613572392397763f8aa1
Kostas Karachalios <kostas.karachalios@gmail.com>
2012-02-26 14:14:10 +0200

    Added Character#check method to calculate ability and skill checks

--- app/models/character.rb
+++ app/models/character.rb
@@ -17,11 +17,18 @@ class Character
   key :wisdom, AbilityScore
   key :charisma, AbilityScore
 
-  def initiative
-    dexterity.modifier + level_bonus
-  end
-
   def level_bonus
     level.to_i / 2
+  end
+
+  def check(ability)
+    ability = case ability
+    when :initiative
+      :dexterity
+    else
+      ability
+    end
+
+    self.send(ability).modifier + level_bonus
   end
 end
