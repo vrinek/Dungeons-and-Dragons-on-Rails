fad69cd8d7a5a5a37a6dfde0ed39a43d92e0502f
Kostas Karachalios <kostas.karachalios@gmail.com>
2012-04-06 15:02:07 +0300

    Removed half-implemented Powers (will be added in the future)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 DnDoR::Application.routes.draw do
   get "combat/initiative", as: 'initiative'
 
-  resources :powers
   resources :encounters
   resources :character_races
   resources :characters, :only => [:index, :destroy]
