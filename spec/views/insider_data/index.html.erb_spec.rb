eb6ae9d0083c26670c142f5bf2aac8374c142d69
Kostas Karachalios <kostas.karachalios@gmail.com>
2012-05-04 17:03:00 +0300

    [WIP] started styling the insider_data controller

--- spec/views/insider_data/index.html.erb_spec.rb
+++ spec/views/insider_data/index.html.erb_spec.rb
@@ -20,10 +20,8 @@ describe "insider_data/index" do
 
   it "renders a list of insider_data" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Html".to_s, :count => 2
-    assert_select "tr>td", :text => "Data Type".to_s, :count => 2
+
+    assert_select "div.identification", :text => 'Data Type #1', :count => 2
+    assert_select "div.details", :text => "Html".to_s, :count => 2
   end
 end
