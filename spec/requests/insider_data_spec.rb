5205909e83991048a50e99a3027c98d46cf9ebe9
Kostas Karachalios <kostas.karachalios@me.com>
2012-05-18 17:52:02 +0300

    Updated the insider_data request spec

--- spec/requests/insider_data_spec.rb
+++ spec/requests/insider_data_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe "Insider Data" do
+  let(:datum) do
+    FactoryGirl.create :insider_datum
+  end
+
   describe "GET /insider_data" do
+    before { datum }
+
     it "works!" do
       get insider_data_path
       response.status.should be(200)
