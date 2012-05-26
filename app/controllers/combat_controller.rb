80dea4c03494b2398102c538726be0d04ff53b07
Κώστας Καραχάλιος <kostas.karachalios@me.com>
2012-05-26 12:25:47 +0300

    Added encounter name and total xp to combat#initiative

--- app/controllers/combat_controller.rb
+++ app/controllers/combat_controller.rb
@@ -1,8 +1,8 @@
 class CombatController < ApplicationController
   def initiative
     if params[:encounter_id].present?
-      encounter = Encounter.find(params[:encounter_id])
-      @initiatives = encounter.roll_initiative.map{|k,v| "#{k} #{v}"}
+      @encounter = Encounter.find(params[:encounter_id])
+      @initiatives = @encounter.roll_initiative.map{|k,v| "#{k} #{v}"}
     end
   end
 end
