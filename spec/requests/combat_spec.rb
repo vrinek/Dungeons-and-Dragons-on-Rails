0f3424aa8350a461f9cc458542793996a6908f07
Kostas Karachalios <kostas.karachalios@me.com>
2012-06-08 14:24:04 +0300

    Added more request specs fors combat controller

--- spec/requests/combat_spec.rb
+++ spec/requests/combat_spec.rb
@@ -2,9 +2,28 @@ require 'spec_helper'
 
 describe "Combat" do
   describe "GET /combat/initiative" do
-    it "works!" do
-      get initiative_path
-      response.status.should be(200)
+    context "without an encounter" do
+      it "responds with statu 200" do
+        get initiative_path
+
+        response.status.should be(200)
+      end
+    end
+
+    context "with an encounter" do
+      let(:encounter) { FactoryGirl.create(:encounter, :with_npcs) }
+
+      it "responds with statu 200" do
+        get initiative_path(encounter_id: encounter.id)
+
+        response.status.should be(200)
+      end
+
+      it "includes the encoutner's name in the body" do
+        get initiative_path(encounter_id: encounter.id)
+
+        response.body.should include(encounter.name)
+      end
     end
   end
 end
