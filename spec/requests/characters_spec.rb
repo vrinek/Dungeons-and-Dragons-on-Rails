b592749519d40807dc1ede229bba11e5294e91ed
Kostas Karachalios <kostas.karachalios@me.com>
2012-06-22 14:09:38 +0300

    Added request specs for DELETE /characters/:id

--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -12,4 +12,34 @@ describe "Characters" do
       response.status.should be(200)
     end
   end
+
+  describe "DELETE /characters/:id" do
+    let(:character_to_delete) { FactoryGirl.create :player }
+
+    before(:each) do
+      FactoryGirl.create_list(:player, 2)
+      FactoryGirl.create_list(:npc, 5)
+      character_to_delete
+    end
+
+    it "works!" do
+      delete character_path(character_to_delete)
+    end
+
+    it "deletes the character" do
+      expect {
+        delete character_path(character_to_delete)
+      }.to change {
+        Character.where(id: character_to_delete.id).first
+      }.to(nil)
+    end
+
+    it "deletes only one character" do
+      expect {
+        delete character_path(character_to_delete)
+      }.to change {
+        Character.count
+      }.by(-1)
+    end
+  end
 end
